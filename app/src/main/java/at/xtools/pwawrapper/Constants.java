120cda26a3810b24d116223c72ded1b52a9115ae
Muzammil Masoomi <106257478+Jumbo810@users.noreply.github.com>
2022-08-18 22:18:14 +0500

    Intent Url Removed

--- app/src/main/java/at/xtools/pwawrapper/Constants.java
+++ app/src/main/java/at/xtools/pwawrapper/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public Constants(){}
     // Root page
     public static String WEBAPP_URL = "localhost:8080";
-    public static String WEBAPP_HOST = "localhost:8080"; // used for checking Intent-URLs
+    public static String WEBAPP_HOST = "https://"; // used for checking Intent-URLs
 
 	// User Agent tweaks
     public static boolean POSTFIX_USER_AGENT = true; // set to true to append USER_AGENT_POSTFIX to user agent
